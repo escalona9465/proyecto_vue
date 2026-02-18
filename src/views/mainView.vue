e336a8645eb0ad4de9bfbf6ca257e32e40806292
jose escalona <escalona9465@gmail.com>
2026-02-18 14:24:57 -0400

    RouterLink 1

---
+++ src/views/mainView.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="container">
+    <Sidebar></Sidebar>
+    <div class="content">
+        <RouterView></RouterView>
+    </div>
+</div>
+</template>
+<script setup>
+import Sidebar from '@/components/sidebar.vue';
+import sidebar from '@/components/sidebar.vue';
+</script>
+<style>
+.container {
+    display: flex;
+}
+.content {
+    flex: 1;
+    padding: 20px;
+}
+</style>
